fac70e04979811bb488453c7dae2a7404597ea3a
taikin12 <k929933@kansai-u.ac.jp>
2020-12-31 13:47:17 +0900

    hi

--- keyboards/MEGAsplit/MEGAsplit.h
+++ keyboards/MEGAsplit/MEGAsplit.h
@@ -27,8 +27,11 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, \
-    k10,  k12    \
+    L00, L01, L02, L03, L04, L05, L06, L07 \
+    L10, L11, L12, L13, L14, L15, L16, L17 \
+    L20, L21, L22, L23, L24, L25, L26, L27 \
+    L30, L31, L32, L33, L34, L35, L36, L37 \
+    L40, L31, L32, L33, L34, L35, L36, L37 \
 ) { \
     { k00, k01,   k02 }, \
     { k10, KC_NO, k12 }  \
