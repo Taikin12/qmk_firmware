4f74f1a0576fcf671b11a78a1a04fade4c4c253e
taikin12 <k929933@kansai-u.ac.jp>
2020-12-31 16:28:08 +0900

    fit; KC T -> KC_T

--- keyboards/MEGAsplit/MEGAsplit.h
+++ keyboards/MEGAsplit/MEGAsplit.h
@@ -27,7 +27,6 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    c7 \
     L00, L01, L02, L03, L04, L05, L06, L07, \
     L10, L11, L12, L13, L14, L15, L16, L17, \
     L20, L21, L22, L23, L24, L25, L26, L27, \
