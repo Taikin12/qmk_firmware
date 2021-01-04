a44d1bc45315256c955c8ad73966186c97012def
taikin12 <k929933@kansai-u.ac.jp>
2021-01-04 17:30:11 +0900

    add Func

--- keyboards/MEGAsplit/keymaps/default/keymap.c
+++ keyboards/MEGAsplit/keymaps/default/keymap.c
@@ -31,12 +31,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_ESC , KC_1   , KC_2 , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , \
+        KC_ESC ,  KC_1  , KC_2 , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , \
         KC_VOLU, KC_TAB , KC_Q , KC_W , KC_E , KC_R , KC_T , KC_8 , \
         KC_VOLD, KC_RSFT, KC_A , KC_S , KC_D , KC_F , KC_G , KC_9 , \
             KC_LSFT     , KC_Z , KC_X , KC_C , KC_V , KC_B , KC_0 , \
-        KC_LGUI , KC_LCTRL , KC_LALT , KC_LALT , KC_SPC , KC_PENT , HOME
+        KC_LGUI , KC_LCTRL , KC_LALT , KC_RALT , KC_SPC , LT(_func, KC_PENT) , HOME
     ),
+    /* Function */
+    [_Func] = LAYOUT(
+        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7   , KC_F8  , \
+        ______ , ______ , ______ , ______ , ______ , ______ , _______ , KC_F9  , \
+        ______ , ______ , ______ , ______ , ______ , ______ , _______ , KC_F10 , \
+            ______      , ______ , ______ , ______ , ______ , _______ , KC_F11 , \
+        ______ , ______ , ______ , ______ , ______ , ______ , KC_F12
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -59,9 +67,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case HOME:
             if (record->event.pressed) {
-                SEND_STRING("/home");
+                SEND_STRING("/");
             } else {
-
+                SEND_STRING("home\n");
             }
             break;
     }
