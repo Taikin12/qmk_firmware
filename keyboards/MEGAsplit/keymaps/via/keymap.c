1bb812b9e5ac4563f9e6e3cdcdfca78584fa5b32
Taikin12 <taikin12@outlook.jp>
2021-06-23 00:13:06 +0900

    add via file

---
+++ keyboards/MEGAsplit/keymaps/via/keymap.c
@@ -0,0 +1,101 @@
+/* Copyright 2020 Taikin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _L1,
+    _L2,
+    _L3,
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    QMKBEST = SAFE_RANGE,
+    QMKURL,
+    HOME,
+    HI
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_ESC   ,  KC_1  , KC_2 , KC_3 , KC_4 , KC_5 , KC_6 , KC_7 , \
+        KC_PGDN  , KC_PGUP, KC_Q , KC_W , KC_E , KC_R , KC_T , KC_8 , \
+        KC_DELETE, KC_RSFT, KC_A , KC_S , KC_D , KC_F , KC_G , KC_9 , \
+             KC_LSFT      , KC_Z , KC_X , KC_C , KC_V , KC_B , KC_0 , \
+        KC_LCTL , KC_MHEN , KC_TAB , KC_LALT , KC_SPC , LT(_Func, KC_PENT) , HOME
+    ),
+    /* Function */
+    [_L1] = LAYOUT(
+        KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8  , \
+        KC_VOLU , KC_VOLU , _______ , _______ , _______ , _______ , _______ , KC_F9  , \
+        HI      , _______ , _______ , _______ , _______ , _______ , _______ , KC_F10 , \
+            _______       , _______ , _______ , _______ , _______ , _______ , KC_F11 , \
+        _______ , _______ , _______ , _______ , _______ , _______ , KC_F12
+    ),
+    [_L2] = LAYOUT(
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+            _______       , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______
+    ),
+    [_L3] = LAYOUT(
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______, \
+            _______       , _______ , _______ , _______ , _______ , _______ , _______, \
+        _______ , _______ , _______ , _______ , _______ , _______ , _______
+    ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QMKBEST:
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("QMK is the best thing ever!");
+            } else {
+                // when keycode QMKBEST is released
+            }
+            break;
+        case QMKURL:
+            if (record->event.pressed) {
+                // when keycode QMKURL is pressed
+                SEND_STRING("https://qmk.fm/\n");
+            } else {
+                // when keycode QMKURL is released
+            }
+            break;
+        case HOME:
+            if (record->event.pressed) {
+                SEND_STRING("/");
+            } else {
+                SEND_STRING("home\n");
+            }
+            break;
+        case HI:
+            if (record->event.pressed) {
+                SEND_STRING("t");
+            } else {
+                SEND_STRING("hi");
+            }
+            break;
+    }
+    return true;
+}
